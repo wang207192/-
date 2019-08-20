00a4bbb209fb1a1f4a29c137aa34700ba6c98e16
wang207192 <1029015834@qq.com>
2019-08-20 16:21:02 +0800

    Signed-off-by: wang207192 <1029015834@qq.com>

---
+++ MaterialEvaluationCal/MaterialEvaluationCal/Calculates/GGHJ/JGJ18-2012.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using MaterialEvaluationCal.Common;
+
+namespace MaterialEvaluationCal.Calculates
+{
+    public class JGJ18_2012
+    {
+        Methods methods = new Methods();
+        public bool Calc(IDictionary<string, IList<IDictionary<string, string>>> dataExtra, ref IDictionary<string, IDictionary<string, IList<IDictionary<string, string>>>> retData, ref string err)
+        {
+            /************************ 代码开始 *********************/
+            return true;
+        }
+
+      
+    }
+}
