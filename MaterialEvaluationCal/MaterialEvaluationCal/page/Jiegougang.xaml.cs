1a9c58a4798e4f5820cdf80bb44931e95e2c5525
chenjy <123535583@qq.com>
2019-08-21 16:05:24 +0800

    结构钢编辑页面

--- MaterialEvaluationCal/MaterialEvaluationCal/page/Jiegougang.xaml.cs
+++ MaterialEvaluationCal/MaterialEvaluationCal/page/Jiegougang.xaml.cs
@@ -10,24 +10,24 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace MaterialEvaluationCal
 {
     /// <summary>
-    /// MainWindow.xaml 的交互逻辑
+    /// Window1.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Jiegougang : Window
     {
-        public MainWindow()
+        public Jiegougang()
         {
-            InitializeComponent();
+
+            //InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Btn_ckbg_Click(object sender, RoutedEventArgs e)
         {
-
+            
         }
     }
 }
